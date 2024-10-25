2440e2712cf880bc35e898804e48281c513a2bf4
dimitar.kyuchukov <dimitar.kyuchukov@egt-digital.com>
2024-10-25 10:17:46 +0300

    Refactor project structure and styling

--- FITUI/src/main.tsx
+++ FITUI/src/main.tsx
@@ -2,13 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './styles/index.css'
+import App from './pages/App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>,
 )
