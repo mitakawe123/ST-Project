0c057680e861294fa191ff8aa9563bdbd0961a40
mitakawe123 <dkyuchukov2003@gmail.com>
2024-12-07 21:29:17 +0200

    test for comments

---
+++ FITAPI.UnitTests/Comments/DeleteCommentEndpointTests.cs
@@ -0,0 +1,29 @@
+using FakeItEasy;
+using FastEndpoints;
+using FITAPI.Application.DTOs.Requests.Comments;
+using FITAPI.Application.Services.Comments;
+using FITAPI.Endpoints.Comments;
+
+namespace FITAPI.UnitTests.Comments
+{
+    public class DeleteCommentEndpointTests
+    {
+        [Fact]
+        public async Task HandleAsync_ShouldDeleteCommentSuccessfully()
+        {
+            var commentsService = A.Fake<ICommentsService>();
+            var ep = Factory.Create<DeleteCommentEndpoint>(commentsService);
+
+            var deleteCommentRequest = new DeleteCommentReqeust(1); 
+
+            A.CallTo(() => commentsService.DeleteCommentAsync(A<DeleteCommentReqeust>.Ignored))
+                .Returns(Task.CompletedTask);
+
+            await ep.HandleAsync(deleteCommentRequest, default);
+
+            A.CallTo(() => commentsService.DeleteCommentAsync(A<DeleteCommentReqeust>.Ignored)).MustHaveHappenedOnceExactly();
+            Assert.False(ep.ValidationFailed);  
+            Assert.Equal("Successfully deleted comment", ep.Response); 
+        }
+    }
+}
---
+++ FITAPI.UnitTests/Comments/UpdateCommentEndpointTests.cs
@@ -0,0 +1,29 @@
+using FakeItEasy;
+using FastEndpoints;
+using FITAPI.Application.DTOs.Requests.Comments;
+using FITAPI.Application.Services.Comments;
+using FITAPI.Endpoints.Comments;
+
+namespace FITAPI.UnitTests.Comments
+{
+    public class UpdateCommentEndpointTests
+    {
+        [Fact]
+        public async Task HandleAsync_ShouldUpdateCommentSuccessfully()
+        {
+            var commentsService = A.Fake<ICommentsService>();
+            var ep = Factory.Create<UpdateCommentEndpoint>(commentsService);
+
+            var updateCommentRequest = new UpdateCommentRequest(1, "Updated comment content");
+
+            A.CallTo(() => commentsService.UpdateCommentAsync(A<UpdateCommentRequest>.Ignored))
+                .Returns(Task.CompletedTask);
+
+            await ep.HandleAsync(updateCommentRequest, default);
+
+            A.CallTo(() => commentsService.UpdateCommentAsync(A<UpdateCommentRequest>.Ignored)).MustHaveHappenedOnceExactly();
+            Assert.False(ep.ValidationFailed); 
+            Assert.Equal("Successfully updated comment", ep.Response); 
+        }
+    }
+}
